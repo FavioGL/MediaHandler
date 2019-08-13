3c1791aec67e0fbd89317ea4dc9a882b670b5fb1
Favio <hugofavio@gmail.com>
2019-08-12 21:26:46 -0700

    full code

--- src/main/java/com/breakbot/media/model/MediaFile.java
+++ src/main/java/com/breakbot/media/model/MediaFile.java
@@ -4,10 +4,30 @@ import lombok.Data;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
 @Data
 public class MediaFile implements Serializable {
     private String fileName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MediaFile)) return false;
+        MediaFile mediaFile = (MediaFile) o;
+        return getFileName().equals(mediaFile.getFileName()) &&
+                getFileType().equals(mediaFile.getFileType()) &&
+                Objects.equals(getFileFullPath(), mediaFile.getFileFullPath()) &&
+                getFileCreationDate().equals(mediaFile.getFileCreationDate()) &&
+                Objects.equals(getTargetPath(), mediaFile.getTargetPath()) &&
+                Objects.equals(getFileNewName(), mediaFile.getFileNewName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFileName(), getFileType(), getFileCreationDate());
+    }
+
     private String fileType;
     private String fileFullPath;
     private Date fileCreationDate;
